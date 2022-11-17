9b4dca5a327cccf681837de457edb747082e6828
miakim <mia.kim@bclabs.co.kr>
2022-11-17 11:16:53 +0900

    add: add prettierrc

---
+++ prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  $schema: "http://json.schemastore.org/prettierrc",
+  trailingComma: "es5",
+  printWidth: 100,
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  useTabs: false,
+  endOfLine: "lf",
+};
