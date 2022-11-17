f51a0e9bf942802acf2e32b5a8bb9aba5599db41
miakim <mia.kim@bclabs.co.kr>
2022-11-17 10:49:33 +0900

    add: add eslint-config-airbnb-base, typescript rules

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
   extends: [
-    'plugin:react/recommended',
+    './recommends/base',
   ],
-  overrides: [],
+  env: {
+    node: true,
+    browser: true,
+  },
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaVersion: 2019,
     sourceType: 'module'
   },
-  plugins: [
-    'react'
-  ],
   rules: {}
 }
---
+++ typescript.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    './recommends/base',
+    './rules/typescript',
+  ].map((e) => require.resolve(e)),
+  env: {
+    node: true,
+    browser: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  rules: {},
+};
